19fc5ee0344f40a6999c62f128e7ccecccb01a3f
fasih31 <fasih31@gmail.com>
2024-11-24 16:34:14 +0400

    addin certificate

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,39 +1,50 @@
 export const achievementData = {
     bio : "Throughout my career, I have achieved significant milestones, including successful product launches, strategic project management, and fostering strong client relationships. My expertise in Islamic finance and data-driven decision-making has enabled me to develop innovative Shariah-compliant solutions while optimizing product performance. With a proven track record in diverse industries, I remain dedicated to delivering top-notch, customer-centric products that drive growth and exceed expectations.",
     achievements : [
+      {
+        id : 1,
+        title : 'Project Management Professsional',
+        details : 'PMI.',
+            },
+    {
+            id : 2,
+            title : 'Product Management Specialization',
+            details : 'University of Virginia.',
+                },
         {
-            id : 1,
+      
+            id : 3,
             title : 'Digital Product Management Specialization',
             details : 'University of Virginia.',
                 },
         {
-            id : 2,
+            id : 4,
             title : 'Diploma in Fintech',
             details : 'TAIF Digital Institute.',
                   },
         {
-            id : 3,
+            id : 5,
             title : 'Financial Technology (Fintech) Innovations',
             details : 'University of Michigan.',
           },
 
         {
-            id : 3,
+            id : 6,
             title : 'Google Project Management: Specialization',
             details : 'Google.',
           },
         {
-            id : 4,
+            id : 7,
             title : 'Project Management Principles and Practices Specialization',
             details : 'University of California, Irvine - The Paul Merage School of Business.',
           },
         {
-            id : 5,
+            id : 8,
             title : 'Project Management Specialization ',
             details : 'University of Colorado Boulder.',
           },
         {
-            id : 6,
+            id : 9,
             title : 'Google IT Support Specialization',
             details : 'Google.',
           }
