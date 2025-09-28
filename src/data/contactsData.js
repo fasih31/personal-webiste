dca3d9abb299648e64db652a4905531fd6fe0d4e
fasih31 <fasih31@gmail.com>
2025-09-28 12:47:52 +0400

    Delete src directory

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'fasih31@gmail.com, info@fasih.com.pk',
-    phone: '+923362609333',
-    address: 'International City, Dubai, United Arab Emirates ',
-
-    sheetAPI: ''
-}
